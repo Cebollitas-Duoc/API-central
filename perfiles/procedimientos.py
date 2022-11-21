5880ecc3cb20c0454c3a89082b36c24e74fbd196
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-11-20 21:23:21 -0300

    fix: consistency

--- perfiles/procedimientos.py
+++ perfiles/procedimientos.py
@@ -6,16 +6,16 @@ def getSessionProfile(session):
     cursor = connection.cursor()
     r = cursor.callproc("PCK_USUARIOS.P_LEE_PERFIL_DE_SESION", [session, "", "", "", "", "", 0, "", "", "", "", "", 0])
     if r[-1] == 1:
-        data["Email"]     = r[1]
-        data["Name"]      = r[2]
-        data["Name2"]     = r[3]
-        data["LastName"]  = r[4]
-        data["LastName2"] = r[5]
-        data["Rut"]       = r[6]
-        data["Address"]   = r[7]
-        data["Phone"]     = r[8]
-        data["Picture"]   = r[9]
-        data["Permiso"]   = r[10]
+        data["Email"]      = r[1]
+        data["Name"]       = r[2]
+        data["Name2"]      = r[3]
+        data["LastName"]   = r[4]
+        data["LastName2"]  = r[5]
+        data["Rut"]        = r[6]
+        data["Address"]    = r[7]
+        data["Phone"]      = r[8]
+        data["Picture"]    = r[9]
+        data["Permission"] = r[10]
 
     data["ValidSession"] = r[11]
     return data
