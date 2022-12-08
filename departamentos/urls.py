042c3dafe92b43fcf8cf3be683803e411f7cc201
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-12-08 00:55:50 -0300

    feature: listar trabajadores

--- departamentos/urls.py
+++ departamentos/urls.py
@@ -4,11 +4,14 @@ from . import views
 app_name = "departamentos"
 
 urlpatterns = [
-    path('viewdptos/',                      views.ViewDptos,                  name="ViewDptos"),
-    path('viewfotosdpto/<int:idDpto>/',     views.ViewFotosDpto,              name="ViewFotosDpto"),
-    path('viewdpto/<int:idDpto>/',          views.ViewDpto,                   name="ViewDpto"),
+    path('viewdptos/',                  views.ViewDptos,     name="ViewDptos"),
+    path('viewfotosdpto/<int:idDpto>/', views.ViewFotosDpto, name="ViewFotosDpto"),
+    path('viewdpto/<int:idDpto>/',      views.ViewDpto,      name="ViewDpto"),
+    
     path('listservices/<int:idDpto>/',      views.listServices,               name="listServices"),
     path('listservicecategories/',          views.listServiceCategories,      name="listServiceCategories"),
     path('listextraservices/<int:idDpto>/', views.listExtraServices,          name="listExtraServices"),
     path('listextraservicecategories/',     views.listExtraServiceCategories, name="listExtraServiceCategories"),
+
+    path('listWorkers/', views.listWorkers, name="listWorkers"),
 ]
