89ac23c8322563612b33a20a9ff2161ecd93092b
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2022-12-09 18:39:17 -0300

    crear check in de forma automatica

--- pagos/views.py
+++ pagos/views.py
@@ -4,6 +4,7 @@ from identificacion.decorators import *
 from rest_framework.decorators import api_view
 from . import procedimientos
 from reservas.procedimientos import getReserva
+import archivos.functions as Documentos
 import time
 
 # Create your views here.
@@ -15,4 +16,8 @@ def pagarReserva(request):
     reserva["VALORTOTAL"] = cursor["VALORTOTAL"]
     date = int(time.time()) * 1000
     dataPago = procedimientos.pagarReserva(request.data["Id_Estado_Pago"], reserva["VALORTOTAL"], date, request.data["Id_Reserva"])
-    return Response(data={"Success":"El pago ha sido guardado"})
+    if (dataPago[1]):
+        Documentos.createCheckIn(dataPago[0])
+        return Response(data={"Success":"El pago ha sido guardado"})
+    else:
+        return Response(data={"Error":"No se pudo completar el pago de la reserva"})
